d4cca2bef707fe751375a455e47aa36abb8caf53
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-04-28 11:30:49 -0500

    minor fix in models, but association with exercise schema not working

--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,36 @@ const Exercise = require("../models/Exercise");
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
-        .sort({ date: -1 })
+        // .sort({ date: -1 })
         .then(dbTransaction => {
             res.json(dbTransaction);
         })
         .catch(err => {
             res.status(400).json(err);
         });
+});
+
+// router.put("/api/workouts/:id", (req, res) => {
+//     Workout.exercise.update(
+//         {
+//             _id: mongojs.ObjectId(req.params.id)
+//         },
+//         {
+//             $push: {
+                
+//             }
+//         }
+//     )
+// });
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body, (err, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(data);
+        }
+    });
 });
 
 
